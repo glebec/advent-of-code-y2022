02ce0f46b3d7277f465bf792f0bc9e3c11957202
Gabriel Lebec <glebec@gmail.com>
2022-12-01 01:05:23 -0500

    feat: condense day 1

--- src/AoC/Day01/Solution.hs
+++ src/AoC/Day01/Solution.hs
@@ -8,27 +8,16 @@ import Data.List.Split (splitOn)
 takeInventories :: String -> [[Int]]
 takeInventories = (fmap . fmap) read . splitOn [""] . lines
 
--- part 1
-biggestInventory :: [[Int]] -> Int
-biggestInventory = maximum . fmap sum
-
--- part 2
-topThreeInventories :: [[Int]] -> Int
-topThreeInventories = sum . take 3 . sortBy (flip compare) . fmap sum
-
--- Work in progress towards making this repo more of a framework:
-
 day01a :: [[Int]] :->: Int
 day01a = MkSol
     { sParse = Just . takeInventories
-    , sSolve = Just . biggestInventory
+    , sSolve = Just . maximum . fmap sum
     , sPrint = show
     }
 
--- day01b :: TODO
 day01b :: [[Int]] :->: Int
 day01b = MkSol
     { sParse = Just . takeInventories
-    , sSolve = Just . topThreeInventories
+    , sSolve = Just . sum . take 3 . sortBy (flip compare) . fmap sum
     , sPrint = show
     }
