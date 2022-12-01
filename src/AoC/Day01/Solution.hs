3be16527ffcd4b6c4a8109d9e8e6f42eb00aaf9a
Gabriel Lebec <glebec@gmail.com>
2022-12-01 00:23:15 -0500

    feat: 1A

--- src/AoC/Day01/Solution.hs
+++ src/AoC/Day01/Solution.hs
@@ -3,22 +3,27 @@ module AoC.Day01.Solution where
 import AoC.Day01.Input (input)
 import AoC.Solver ((:->:)(..))
 
-exampleNums :: [Int]
-exampleNums = read <$> lines input
+import Data.List.Split (splitOn)
+
+takeInventories :: String -> [[Int]]
+takeInventories = (fmap . fmap) read . splitOn [""] . lines
+
+inventories :: [[Int]]
+inventories = read <$> lines input
 
 -- part 1
-examplePart1 :: [Int] -> Int
-examplePart1 = sum
+part1 :: [[Int]] -> Int
+part1 = maximum . fmap sum
 
 -- part 2
 -- TODO
 
 -- Work in progress towards making this repo more of a framework:
 
-day01a :: [Int] :->: Int
+day01a :: [[Int]] :->: Int
 day01a = MkSol
-    { sParse = Just . fmap read . lines
-    , sSolve = Just . examplePart1
+    { sParse = Just . takeInventories
+    , sSolve = Just . part1
     , sPrint = show
     }
 
