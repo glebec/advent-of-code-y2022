3989fae675dd29a9e71748868a7a73051f787750
Gabriel Lebec <glebec@gmail.com>
2022-12-02 01:12:54 -0500

    feat: day02b

--- src/AoC/Day02/Solution.hs
+++ src/AoC/Day02/Solution.hs
@@ -12,8 +12,8 @@ data Outcome = Loss | Tie | Win deriving (Enum, Show)
 
 type Parser = Parsec Void String
 
-sign :: Parser Sign
-sign =
+pSign :: Parser Sign
+pSign =
     char 'A' $> Rock <|>
     char 'B' $> Paper <|>
     char 'C' $> Scissors <|>
@@ -21,13 +21,42 @@ sign =
     char 'Y' $> Paper <|>
     char 'Z' $> Scissors
 
-games :: Parser [(Sign, Sign)]
-games = many $ do
-    elfSign <- sign
+pOutcome :: Parser Outcome
+pOutcome =
+    char 'X' $> Loss <|>
+    char 'Y' $> Tie <|>
+    char 'Z' $> Win
+
+gamesA :: Parser [(Sign, Sign)]
+gamesA = many $ do
+    elfSign <- pSign
     space
-    mySign <- sign
+    mySign <- pSign
     space
     pure (elfSign, mySign)
+
+gamesB :: Parser [(Sign, Outcome)]
+gamesB = many $ do
+    elfSign <- pSign
+    space
+    outcome <- pOutcome
+    space
+    pure (elfSign, outcome)
+
+winnerAgainst :: Sign -> Sign
+winnerAgainst Rock = Paper
+winnerAgainst Paper = Scissors
+winnerAgainst Scissors = Rock
+
+losesTo :: Sign -> Sign
+losesTo Rock = Scissors
+losesTo Scissors = Paper
+losesTo Paper = Rock
+
+choose :: Outcome -> Sign -> Sign
+choose Win s = winnerAgainst s
+choose Loss s = losesTo s
+choose Tie s = s
 
 play :: (Sign, Sign) -> Outcome
 play game@(elf, me) = case game of
@@ -46,19 +75,23 @@ outcomeValue = \case
   Tie -> 3
   Win -> 6
 
-roundScore :: (Sign, Sign) -> Int
-roundScore game@(_, me) = signValue me + outcomeValue (play game)
+roundScoreA :: (Sign, Sign) -> Int
+roundScoreA game@(_, me) = signValue me + outcomeValue (play game)
+
+roundScoreB :: (Sign, Outcome) -> Int
+roundScoreB (elfSign, outcome) =
+    signValue (choose outcome elfSign) + outcomeValue outcome
 
 day02a :: [(Sign, Sign)] :->: Int
 day02a = MkSol
-    { sParse = parseMaybe games
-    , sSolve = Just . sum . fmap roundScore
+    { sParse = parseMaybe gamesA
+    , sSolve = Just . sum . fmap roundScoreA
     , sPrint = show
     }
 
--- day01b :: [(Sign, Sign)] :->: Int
--- day01b = MkSol
---     { sParse = parseMaybe games
---     , sSolve = Just . undefined
---     , sPrint = show
---     }
+day02b :: [(Sign, Outcome)] :->: Int
+day02b = MkSol
+    { sParse = parseMaybe gamesB
+    , sSolve = Just . sum . fmap roundScoreB
+    , sPrint = show
+    }
