3a47fe9a331952589ee0e184b2a1609093944ad4
Gabriel Lebec <glebec@gmail.com>
2022-12-01 23:41:33 -0500

    feat: day 2 prep

---
+++ src/AoC/Day02/Input.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module AoC.Day02.Input where
+
+import Text.RawString.QQ (r)
+
+input :: String
+input = [r|...|]
---
+++ src/AoC/Day02/Solution.hs
@@ -0,0 +1,17 @@
+module AoC.Day02.Solution where
+
+import AoC.Solver ((:->:)(..))
+
+day01a :: [[Int]] :->: Int
+day01a = MkSol
+    { sParse = Just . undefined
+    , sSolve = Just . undefined
+    , sPrint = show
+    }
+
+day01b :: [[Int]] :->: Int
+day01b = MkSol
+    { sParse = Just . undefined
+    , sSolve = Just . undefined
+    , sPrint = show
+    }
