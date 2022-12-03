6dc681e8229013656dc1cca472ed7ad455b65cd3
Gabriel Lebec <glebec@gmail.com>
2022-12-03 00:54:58 -0500

    chore: day03a cleanup

--- src/AoC/Day03/Solution.hs
+++ src/AoC/Day03/Solution.hs
@@ -7,20 +7,20 @@ import Data.Set qualified as S
 halve :: String -> (String, String)
 halve s = splitAt (length s `div` 2) s
 
-common :: (String, String) -> S.Set Char
-common (l, r) = S.intersection (S.fromList l) (S.fromList r)
+common :: (String, String) -> [Char]
+common (l, r) = S.toList $ S.intersection (S.fromList l) (S.fromList r)
 
 priority :: Char -> Int
 priority c | fromEnum c >= 97 = fromEnum c - 96  -- [a..z]
         | fromEnum c >= 65 = fromEnum c - 38  -- [A..Z]
         | otherwise = 0
 
-sumPriorities :: [String] -> Int
-sumPriorities = sum . fmap (sum . fmap priority . S.toList . common . halve)
+sackValue :: String -> Int
+sackValue = sum . fmap priority . common . halve
 
 day03a :: [String] :->: Int
 day03a = MkSol
     { sParse = Just . lines
-    , sSolve = Just . sumPriorities
+    , sSolve = Just . sum . fmap sackValue
     , sPrint = show
     }
