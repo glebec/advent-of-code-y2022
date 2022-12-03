964590ad5d86a71bfcebed95aff3e18488d44384
Gabriel Lebec <glebec@gmail.com>
2022-12-03 01:20:09 -0500

    chore: refactor day03

--- src/AoC/Day03/Solution.hs
+++ src/AoC/Day03/Solution.hs
@@ -5,36 +5,33 @@ import AoC.Solver ((:->:)(..))
 import Data.List.Split (chunksOf)
 import Data.Set qualified as S
 
-halve :: String -> (String, String)
-halve s = splitAt (length s `div` 2) s
+letters :: S.Set Char
+letters = S.fromList $ ['A'..'Z'] ++ ['a'..'z']
 
-common2 :: (String, String) -> [Char]
-common2 (l, r) = S.toList $ S.intersection (S.fromList l) (S.fromList r)
+common :: [String] -> [Char]
+common sacks = S.toList $ foldr S.intersection letters (fmap S.fromList sacks)
+
+halve :: String -> [String]
+halve s = let (l, r) = splitAt (length s `div` 2) s in [l, r]
 
 priority :: Char -> Int
 priority c | fromEnum c >= 97 = fromEnum c - 96  -- [a..z]
            | fromEnum c >= 65 = fromEnum c - 38  -- [A..Z]
            | otherwise = 0
 
-sackValue :: String -> Int
-sackValue = sum . fmap priority . common2 . halve
+commonValue :: [String] -> Int
+commonValue = sum . fmap priority . common
 
 day03a :: [String] :->: Int
 day03a = MkSol
     { sParse = Just . lines
-    , sSolve = Just . sum . fmap sackValue
+    , sSolve = Just . sum . fmap (commonValue . halve)
     , sPrint = show
     }
-
-letters :: S.Set Char
-letters = S.fromList $ ['A'..'Z'] ++ ['a'..'z']
-
-common3 :: [String] -> [Char]
-common3 sacks = S.toList $ foldr S.intersection letters (fmap S.fromList sacks)
 
 day03b :: [[String]] :->: Int
 day03b = MkSol
     { sParse = Just . chunksOf 3 . lines
-    , sSolve = Just . sum . fmap (sum . fmap priority . common3)
+    , sSolve = Just . sum . fmap commonValue
     , sPrint = show
     }
