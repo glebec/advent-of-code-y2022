5e8cbc3a17a14878313adb7cdb35d5d061357f8b
Gabriel Lebec <glebec@gmail.com>
2022-12-03 01:13:28 -0500

    feat: day03b

--- src/AoC/Day03/Solution.hs
+++ src/AoC/Day03/Solution.hs
@@ -2,13 +2,14 @@ module AoC.Day03.Solution where
 
 import AoC.Solver ((:->:)(..))
 
+import Data.List.Split (chunksOf)
 import Data.Set qualified as S
 
 halve :: String -> (String, String)
 halve s = splitAt (length s `div` 2) s
 
-common :: (String, String) -> [Char]
-common (l, r) = S.toList $ S.intersection (S.fromList l) (S.fromList r)
+common2 :: (String, String) -> [Char]
+common2 (l, r) = S.toList $ S.intersection (S.fromList l) (S.fromList r)
 
 priority :: Char -> Int
 priority c | fromEnum c >= 97 = fromEnum c - 96  -- [a..z]
@@ -16,11 +17,24 @@ priority c | fromEnum c >= 97 = fromEnum c - 96  -- [a..z]
            | otherwise = 0
 
 sackValue :: String -> Int
-sackValue = sum . fmap priority . common . halve
+sackValue = sum . fmap priority . common2 . halve
 
 day03a :: [String] :->: Int
 day03a = MkSol
     { sParse = Just . lines
     , sSolve = Just . sum . fmap sackValue
+    , sPrint = show
+    }
+
+letters :: S.Set Char
+letters = S.fromList $ ['A'..'Z'] ++ ['a'..'z']
+
+common3 :: [String] -> [Char]
+common3 sacks = S.toList $ foldr S.intersection letters (fmap S.fromList sacks)
+
+day03b :: [[String]] :->: Int
+day03b = MkSol
+    { sParse = Just . chunksOf 3 . lines
+    , sSolve = Just . sum . fmap (sum . fmap priority . common3)
     , sPrint = show
     }
