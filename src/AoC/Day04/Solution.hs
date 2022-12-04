3e0146d2f6f756fa9f7bfe879bb44f0ad735592d
Gabriel Lebec <glebec@gmail.com>
2022-12-04 01:49:27 -0500

    feat: day04b

--- src/AoC/Day04/Solution.hs
+++ src/AoC/Day04/Solution.hs
@@ -2,27 +2,39 @@ module AoC.Day04.Solution where
 
 import AoC.Solver ((:->:)(..))
 
-import Numeric.Interval.NonEmpty (Interval, (...), contains)
 import Data.Void (Void)
 import Text.Megaparsec (Parsec, many, parseMaybe)
 import Text.Megaparsec.Char (char, space)
 import Text.Megaparsec.Char.Lexer (decimal)
 
 type Parser = Parsec Void String
-type Sections = Interval Int
+type Sections = (Int, Int)
 
-rangePair :: Parser (Sections, Sections)
-rangePair = do
-    r1 <- (...) <$> (decimal <* char '-') <*> (decimal <* char ',')
-    r2 <- (...) <$> (decimal <* char '-') <*> (decimal <* space)
-    pure (r1, r2)
+sectionPair :: Parser (Sections, Sections)
+sectionPair = do
+    s1 <- (,) <$> (decimal <* char '-') <*> (decimal <* char ',')
+    s2 <- (,) <$> (decimal <* char '-') <*> (decimal <* space)
+    pure (s1, s2)
 
-eitherContains :: Ord a => (Interval a, Interval a) -> Bool
+eitherContains :: (Sections, Sections) -> Bool
 eitherContains (a, b) = a `contains` b || b `contains` a
+    where contains (xLo, xHi) (yLo, yHi) = xLo <= yLo && yHi <= xHi
 
-day04a :: [(Sections, Sections)] :->: Int
-day04a = MkSol
-    { sParse = parseMaybe $ many rangePair
-    , sSolve = Just . length . filter eitherContains
+overlaps :: (Sections, Sections) -> Bool
+overlaps pair@(a, b) =
+    eitherContains pair
+    || a `lowOverlaps` b
+    || b `lowOverlaps` a
+    where lowOverlaps (xLo, xHi) (yLo, _) = xLo <= yLo && yLo <= xHi
+
+day04 :: ((Sections, Sections) -> Bool)  -- predicate on pairs
+      -> ([(Sections, Sections)] :->: Int)  -- solution
+day04 predicate = MkSol
+    { sParse = parseMaybe $ many sectionPair
+    , sSolve = Just . length . filter predicate
     , sPrint = show
     }
+
+day04a, day04b :: [(Sections, Sections)] :->: Int
+day04a = day04 eitherContains
+day04b = day04 overlaps
