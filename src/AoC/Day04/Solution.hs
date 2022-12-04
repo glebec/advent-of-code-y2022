6b7cbb0d9067c074714d2e7e74322d18d89c0f44
Gabriel Lebec <glebec@gmail.com>
2022-12-04 02:35:17 -0500

    feat: day04 switch to library

--- src/AoC/Day04/Solution.hs
+++ src/AoC/Day04/Solution.hs
@@ -2,30 +2,20 @@ module AoC.Day04.Solution where
 
 import AoC.Solver ((:->:)(..))
 
+import Data.IntegerInterval (IntegerInterval, (<=..<=), (==?), isSubsetOf)
 import Data.Void (Void)
 import Text.Megaparsec (Parsec, many, parseMaybe)
 import Text.Megaparsec.Char (char, space)
 import Text.Megaparsec.Char.Lexer (decimal)
 
 type Parser = Parsec Void String
-type Sections = (Int, Int)
+type Sections = IntegerInterval
 
 sectionPair :: Parser (Sections, Sections)
 sectionPair = do
-    s1 <- (,) <$> (decimal <* char '-') <*> (decimal <* char ',')
-    s2 <- (,) <$> (decimal <* char '-') <*> (decimal <* space)
+    s1 <- (<=..<=) <$> (decimal <* char '-') <*> (decimal <* char ',')
+    s2 <- (<=..<=) <$> (decimal <* char '-') <*> (decimal <* space)
     pure (s1, s2)
-
-eitherContains :: (Sections, Sections) -> Bool
-eitherContains (a, b) = a `contains` b || b `contains` a
-    where contains (xLo, xHi) (yLo, yHi) = xLo <= yLo && yHi <= xHi
-
-overlaps :: (Sections, Sections) -> Bool
-overlaps pair@(a, b) =
-    eitherContains pair
-    || a `lowOverlaps` b
-    || b `lowOverlaps` a
-    where lowOverlaps (xLo, xHi) (yLo, _) = xLo <= yLo && yLo <= xHi
 
 day04 :: ((Sections, Sections) -> Bool)  -- predicate on pairs
       -> ([(Sections, Sections)] :->: Int)  -- solution
@@ -36,5 +26,5 @@ day04 predicate = MkSol
     }
 
 day04a, day04b :: [(Sections, Sections)] :->: Int
-day04a = day04 eitherContains
-day04b = day04 overlaps
+day04a = day04 $ \(a, b) -> a `isSubsetOf` b || b `isSubsetOf` a
+day04b = day04 $ uncurry (==?)
