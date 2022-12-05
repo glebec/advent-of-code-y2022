0bcb456d72a7f919ae8e2ebcddd8bfe83f8bc376
Gabriel Lebec <glebec@gmail.com>
2022-12-05 01:34:11 -0500

    feat: day 00 template

---
+++ src/AoC/Day00/Input.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module AoC.Day00.Input where
+
+import Text.RawString.QQ (r)
+
+input :: String
+input = [r|example|]
---
+++ src/AoC/Day00/Solution.hs
@@ -0,0 +1,10 @@
+module AoC.Day00.Solution where
+
+import AoC.Solver ((:->:)(..))
+
+day00a :: Int :->: Int
+day00a = MkSol
+    { sParse = Just . undefined
+    , sSolve = Just . undefined
+    , sPrint = show
+    }
